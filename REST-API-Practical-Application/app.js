d6723bab356d6ecabd62bb330ddeb357bcfccb4f
mayank <mayankece155@gmail.com>
2020-05-15 21:53:38 +0530

    Rest-API project in  node js

---
+++ REST-API-Practical-Application/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+
+mongoose
+  .connect(
+    'mongodb+srv://mayank:mayanksharma@cluster0-jjiei.mongodb.net/messages?retryWrites=true&w=majority',
+        {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        }
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
